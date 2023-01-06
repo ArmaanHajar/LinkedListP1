225a0986fcb68ae9f1d9d9bb015b8d31455ad3c1
ArmaanHajar <armaanhajar@icloud.com>
2023-01-06 14:25:40 -0800

    almost done

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "Node.h"
+#include "Student.h"
+#include <cstring>
 
 using namespace std;
 
 int main() {
-  
+  char name[50] = "";
+
+  strncpy(name, "Armaan");
+  Student* armaan = new Student(name);
+  Node* node1 = new Node(armaan);
+
+  strncpy(name, "Jeff");
+  Student* jeff = new Student(name);
+  Node* node2 = new Node(jeff);
+
+  strncpy(name, "Galbraith");
+  Student* galbraith = new Student(name);
+  Node* node3 = new Node(galbraith);
+
+  node1->setNext(node2);
+  node2->setNext(node3);
+  node3->setNext(NULL);
+
+  cout << node1->getStudent()->name << endl;
+  cout << node1->getNext()->name << endl;
+  cout << node2->getNext()->name << endl;
 }
